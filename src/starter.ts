2f4f290e95630bcfbbca567b08d4df206aa12715
cijin <cijininurhood@gmail.com>
2021-01-27 08:06:32 +0700

    adds starter files to start development

---
+++ src/starter.ts
@@ -0,0 +1,8 @@
+// @TODO remove this file once there is a file with some code in it.
+const a = 'this file just makes ts happy'
+const b = 'though delte this once there is'
+const c = 'another .ts or .ts* file'
+
+export function blah() {
+  return a + b + c
+}
