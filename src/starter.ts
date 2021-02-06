1d983392806faeb51bb4ae5b437bf09103bc0ab1
cijin <cijininurhood@gmail.com>
2021-02-06 17:52:28 +0700

    switch from next to parcel

--- src/starter.ts
+++
@@ -1,8 +0,0 @@
-// @TODO remove this file once there is a file with some code in it.
-const a = 'this file just makes ts happy'
-const b = 'though delte this once there is'
-const c = 'another .ts or .ts* file'
-
-export function blah() {
-  return a + b + c
-}
